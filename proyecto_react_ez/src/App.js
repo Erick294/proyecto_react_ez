a8d3aa02efd55338480248d5f28b6df0bc556fbf
Erick Zambrano <beowulfblackman@hotmail.com>
2022-06-29 04:19:01 -0500

    Tarea 7

---
+++ proyecto_react_ez/src/App.js
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hola Mundo en React</h1>
+        
+        <h3>Expresión: 1-1</h3>
+        <p>Resultado: {1-1}</p>
+
+        <h3>Arreglo:</h3>
+        <p>{[1, 2, 3, 4, 5]}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
